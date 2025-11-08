bc96c3e56e8f43872eb2ed181cfbbd840e772b68
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-11-08 15:16:28 -0300

    create endpoints for retrieving tickets

--- Backend/ServiceDesk.Api/Controllers/TicketsController.cs
+++ Backend/ServiceDesk.Api/Controllers/TicketsController.cs
@@ -4,12 +4,44 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class TicketController : ControllerBase
 {
+    private readonly GetTicketsHandler _getAll;
+    private readonly GetTicketByIdHandler _getById;
     private readonly CreateTicketHandler _create;
 
-    public TicketController(CreateTicketHandler create) => _create = create;
+    public TicketController(
+        GetTicketByIdHandler getById,
+        GetTicketsHandler getAll,
+        CreateTicketHandler create
+    )
+    {
+        _getById = getById;
+        _getAll = getAll;
+        _create = create;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(
+        [FromQuery] bool includeClosed = true,
+        CancellationToken ct = default
+    )
+    {
+        var tickets = await _getAll.Handle(new GetTicketsQuery(includeClosed), ct);
+
+        return Ok(tickets);
+    }
 
     [HttpGet("{id:int}")]
-    public IActionResult Get(int id) => Ok(new { id });
+    public async Task<IActionResult> GetById(int id, CancellationToken ct = default)
+    {
+        var ticket = await _getById.Handle(new GetTicketByIdQuery(id), ct);
+
+        if (ticket is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(ticket);
+    }
 
     [HttpPost]
     public async Task<IActionResult> Create(
@@ -18,6 +50,6 @@ public class TicketController : ControllerBase
     )
     {
         var id = await _create.Handle(command, ct);
-        return CreatedAtAction(nameof(Get), new { id }, new { id });
+        return CreatedAtAction(nameof(GetById), new { id });
     }
 }
